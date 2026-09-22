ebef8f061cd20ad7f2063fd500c5133af48926f2
taquang.nhut <taquangnhut7@gmail.com>
2026-09-22 13:38:18 +0700

    cac biding

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnDisplay.setOnClickListener {
+
+            val name = binding.edtName.text.toString()
+            val mssv = binding.edtMssv.text.toString()
+
+            binding.tvResult.text =
+                "Họ tên: $name\nMSSV: $mssv"
+        }
+    }
+}
